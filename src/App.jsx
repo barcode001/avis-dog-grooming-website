6259239df51af76e0b39c9e19f19c24f0560f3a7
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-06-02 23:52:08 -0500

    Add basic App.jsx to fix build

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Avi's Dog Grooming Website</h1>
+      <p>This is the beginning of something awesome 🐾</p>
+    </div>
+  );
+}
