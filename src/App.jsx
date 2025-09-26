5b1bb130e80a4bea253c35f6b284877034be4971
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-09-26 07:30:35 -0500

    change title, meta tags and How it works

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ export default function App() {
               </>
             }
           />
-          {/* <Route path="/" element={<Home />} /> */}
+
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
--- src/pages/HowItWorks.jsx
+++ src/pages/HowItWorks.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import useScrollReveal from "../hooks/useScrollReveal";
+
 export default function HowItWorks() {
   useScrollReveal();
   return (
@@ -11,13 +12,12 @@ export default function HowItWorks() {
             <strong>1.</strong> Book an appointment online or by phone.
           </li>
           <li className="fade-up">
-            <strong>2.</strong> House Call – I come to you fully equipped, just
-            provide a space with an outlet. I’ll groom your pup and clean up
-            everything before I leave.
+            <strong>2.</strong> Bring your pup to Avi’s Little Groom Room at the
+            scheduled time.
           </li>
           <li className="fade-up">
-            <strong> 3.</strong> Your dog gets pampered without you ever leaving
-            home.
+            <strong>3.</strong> Your dog gets pampered in a safe and loving home
+            environment, and you pick them up fresh and clean.
           </li>
         </ol>
       </div>
